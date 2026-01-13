a6e7690d0fb29b486c4af6f9999a2c7d3b8578a5
GopalVerma1303 <vgopaldps@gmail.com>
2026-01-13 12:05:37 +0530

    fixed app not loading on web

---
+++ metro.config.js
@@ -0,0 +1,18 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+// Configure path aliases
+config.resolver = {
+  ...config.resolver,
+  alias: {
+    '@': __dirname,
+  },
+  extraNodeModules: {
+    '@': __dirname,
+  },
+};
+
+module.exports = config;
